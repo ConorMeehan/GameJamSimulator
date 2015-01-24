748156ce0c5aaa21b227bb7e91b6762dfc6e2761
JtheSpaceC <jthespacec@gmail.com>
2015-01-24 11:10:45 +0000

    Uploaded Prefabs: Timer & Jammer
    I also have the scene PICK YOUR TEAM in the Scenes folder, which I'm
    working on. It's level 1

---
+++ Unity/Assets/Scripts/StatsScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatsScript : MonoBehaviour {
+
+	public float codingSkill;
+	public float artSkill;
+	public float audioSkill;
+	public float writingSkill;
+
+	float minimumSkillPoints = 60;
+	float maximumSkillPoints = 90;
+
+	float skillPointsForMe = 60f; //just a default value
+	float skillPointsLeftToAssign;
+
+
+	void Start () 
+	{
+		SetStatsAtBeginning ();
+	}
+
+	void SetStatsAtBeginning()
+	{
+		skillPointsForMe = Random.Range (minimumSkillPoints, maximumSkillPoints);
+		skillPointsLeftToAssign = skillPointsForMe;
+		
+		codingSkill = Random.Range (0, skillPointsLeftToAssign); //first skill roll
+		skillPointsLeftToAssign -= codingSkill; //reduce points left to assign
+		
+		artSkill = Random.Range (0, skillPointsLeftToAssign);
+		skillPointsLeftToAssign -= artSkill;
+		
+		audioSkill = Random.Range (0, skillPointsLeftToAssign);
+		skillPointsLeftToAssign -= audioSkill;
+		
+		writingSkill = skillPointsLeftToAssign; //gets whatever's left
+		
+		Debug.Log (skillPointsForMe+" = "+ codingSkill+" "+ artSkill+" "+ audioSkill+" "+ writingSkill);
+	}
+
+}//end of Mono bracket
---
+++ Unity/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimerScript : MonoBehaviour {
+
+	public static TimerScript timerScript;
+
+	public float timeRemainingAtSceneStart = 2880f; //In Hours. can set in Inspector for testing your scene starting from
+	//a specific time. Don't change here. Change in  editor.
+
+	public float timePassingRate = 16f; //to pass 48hrs (2880 mins) in 3real-world mins, set this to 16, 
+	// Unity measures time in seconds. Don't change here. Change in  editor.
+
+	[HideInInspector]
+	public float timeRemaining; //the time that'll be displayed on the clock. don't need to edit
+
+	public Text timerText;
+
+
+	void Awake()
+	{
+		//this code makes the timer a Singleton. You can place it in any scene and it'll persist into the next
+		//scene without creating two timers (like if the next scene were to have another timer in it)
+		if(timerScript == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			timerScript = this;
+		}
+		else if (timerScript != this)
+		{
+			Destroy(gameObject);
+		}
+		//end of Singleton
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		timeRemaining = timeRemainingAtSceneStart * 60 * 60; //converting hours into seconds for Unity
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//countdown
+		timeRemaining = timeRemaining - (Time.deltaTime*timePassingRate*60);
+		timerText.text = ((timeRemaining /3600).ToString("F")) + " hrs";
+	}
+	
+}//end of MonoBehaviour bracket
