8e34623df58cb32566ebc4ffdec8e83fb7251a8a
JtheSpaceC <jthespacec@gmail.com>
2015-01-24 14:44:28 +0000

    Update Nothing  Significant
    update
    
    nothing significant

---
+++ Unity/Assets/Scripts/LevelStartScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelStartScript : MonoBehaviour {
+
+	public GameObject SplashScreenCanvas;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Time.timeScale = 0;
+	}
+
+	public void BeginLevel()
+	{
+		print ("test");
+		Time.timeScale = 1;
+		SplashScreenCanvas.SetActive (false);
+	}
+
+	public void Test()
+	{
+		print ("test 2");
+	}
+
+}
---
+++ Unity/Assets/Scripts/ignore scripts/ButtonClickScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonClickScript : MonoBehaviour {
+
+	public GameObject SplashScreenCanvas;
+
+
+	void Start()
+	{
+		//Time.timeScale = 0;
+	}
+
+	void OnMouseDown()
+	{
+		Time.timeScale = 1;
+		SplashScreenCanvas.SetActive (false);
+		print ("test me");
+	}
+
+}
