619484223c6a09605d761115ed4eca113920f4cf
MarkCCoder <markc.coder@gmail.com>
2015-01-25 15:37:49 +0000

    Update randomise script to rotate jammers

--- Unity/Assets/Scripts/RandomiseJammer.cs
+++ Unity/Assets/Scripts/RandomiseJammer.cs
@@ -7,5 +7,8 @@ public class RandomiseJammer : MonoBehaviour
 	{
 		Color random = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
 		GetComponent<SpriteRenderer> ().color = random;
+
+		Quaternion rotation = Quaternion.Euler (0, 0, Random.Range (0, 180f));
+		gameObject.transform.localRotation = rotation;
 	}
 }
