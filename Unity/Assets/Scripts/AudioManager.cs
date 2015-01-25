4e814c6c2db9f952b4cbc7276e37ce073daadeec
JtheSpaceC <jthespacec@gmail.com>
2015-01-25 10:23:12 +0000

    update
    added the persistent  audio (and prefab for it) and created a LOSE scne,
    but it's empty.

---
+++ Unity/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	public static AudioManager musicManager;
+	
+	private GameObject player;
+	
+	public bool persistAfterLoad = true;
+	
+	public bool muteMusic = false;
+
+	
+	
+	
+	void Awake()
+	{
+		if(musicManager == null && persistAfterLoad)
+		{
+			DontDestroyOnLoad(gameObject);
+			musicManager = this;
+		}
+		else if(musicManager == null && !persistAfterLoad)
+		{
+			musicManager = this;
+		}
+		else if (musicManager != this)
+		{
+			Destroy(gameObject);
+		}
+		
+	}
+
+
+	public void BecomeDontDestroyOnLoad()
+	{
+		DontDestroyOnLoad (gameObject);
+		musicManager = this;
+		persistAfterLoad = true;
+	}
+	public void BecomeDestroyOnLoad()
+	{
+		DontDestroyOnLoad (gameObject);
+		persistAfterLoad = false;
+	}
+	public void DestroyThisNow()
+	{
+		print ("called");
+		DestroyImmediate (gameObject);
+	}
+	
+}//Mono
